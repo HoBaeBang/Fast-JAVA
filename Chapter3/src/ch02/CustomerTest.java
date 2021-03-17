6a95073927e20c2e7267fe06641ebf25004ead4a
B-aslan0 <bhb0407@naver.com>
2021-03-18 00:05:41 +0900

    일반 손님과 VIP손님 사용하는 main

---
+++ Chapter3/src/ch02/CustomerTest.java
@@ -0,0 +1,22 @@
+package ch02;
+
+public class CustomerTest {
+
+   public static void main(String[] args) {
+	   
+      Customer customerLee = new Customer();
+      customerLee.setCustomerName("Lee");
+      customerLee.setCustomerID(10010);
+      customerLee.bonusPoint = 1000; //기존에 있던 포인트
+      System.out.println(customerLee.showCustomerInfo());
+      
+      
+      VIPCustomer customerKim = new VIPCustomer();
+      customerKim.setCustomerName("Kim");
+      customerKim.setCustomerID(10020);
+      customerKim.bonusPoint = 10000;
+      System.out.println(customerKim.showCustomerInfo());
+   
+   }
+
+}
