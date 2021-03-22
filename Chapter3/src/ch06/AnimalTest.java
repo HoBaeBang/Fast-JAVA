4cc40aa75a09a95654339d7fd40b660288046ebc
B-aslan0 <bhb0407@naver.com>
2021-03-23 00:13:56 +0900

    반복문 과 리스트를 이용한 다운캐스팅 해보기

--- Chapter3/src/ch06/AnimalTest.java
+++ Chapter3/src/ch06/AnimalTest.java
@@ -62,7 +62,7 @@ public class AnimalTest {
 		Animal tAnimal = new Tiger();
 		Animal eAnimal = new Eagle();
 	
-//		AnimalTest test = new AnimalTest();
+		AnimalTest test = new AnimalTest();
 //		test.moveAnimal(hAnimal);
 //		test.moveAnimal(tAnimal);
 //		test.moveAnimal(eAnimal);
@@ -75,7 +75,34 @@ public class AnimalTest {
 		for(Animal animal : animalList) {
 			animal.move();
 		}
+		
+		test.testDownCasting(animalList);
+		
 	}
+	
+	public void testDownCasting(ArrayList<Animal> list) {
+		
+		for(int i =0; i<list.size(); i++) {
+			
+			Animal animal = list.get(i);
+			
+			if( animal instanceof Human ) {
+				Human human = (Human)animal;
+				human.readBook();
+			}
+			if( animal instanceof Tiger ) {
+				Tiger tiger = (Tiger)animal;
+				tiger.hunting();
+			}
+			if( animal instanceof Eagle ) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			}
+			
+			
+		}
+	}
+	
 	
 	public void moveAnimal(Animal animal) {
 		animal.move();
