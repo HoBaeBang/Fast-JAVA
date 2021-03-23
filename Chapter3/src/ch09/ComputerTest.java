09a1a2f91d32624a55494015ab4ace7c9c6595ee
B-aslan0 <bhb0407@naver.com>
2021-03-24 00:14:03 +0900

    추상클래스를 이용해서 클래스를 상속한것을 실행해보자

---
+++ Chapter3/src/ch09/ComputerTest.java
@@ -0,0 +1,13 @@
+package ch09;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+
+		Computer pc = new Desktop();
+//		Computer computer = new Computer();
+		
+			
+	}
+
+}
