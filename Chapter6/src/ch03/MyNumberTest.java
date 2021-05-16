c392492af8d096951a1548bf49c585360fdc2160
B-aslan0 <bhb0407@naver.com>
2021-05-16 23:12:26 +0900

    람다식 복습해보기

--- Chapter6/src/ch03/MyNumberTest.java
+++ Chapter6/src/ch03/MyNumberTest.java
@@ -6,6 +6,7 @@ public class MyNumberTest {
 
 		MyNumber myNumber = (x,y) -> x>y? x:y; //3항 연산자 조건식의 연산결과가 true이면 식1을 결과로 얻고 false이면 식2를 결과로 얻는다.
 		System.out.println(myNumber.getMax(10,20));
+		
 	}
 
 }
