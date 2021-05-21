982be032143c0b28defe5a6e952eb60f75bccfc4
B-aslan0 <bhb0407@naver.com>
2021-05-21 20:13:24 +0900

    LinkedList를 구성하는 노드를 만들기 위한 클래스

--- Chapter5/src/ch03/MyListNode.java
+++ Chapter5/src/ch03/MyListNode.java
@@ -1,21 +1,21 @@
 package ch03;
 
-public class MyListNode {
-	private String data;	//자료
-	public MyListNode next;	//다음 노드를 가리키는 링크
+public class MyListNode {	//LinkedList를 구성하는 노드를 만들기 위한 클래스
+	private String data;	//자료를 넣을 변수 설정
+	public MyListNode next;	//다음 노드를 가리키는 링크 변수 설정
 	
 	public MyListNode() {	//디폴트 생성자
-		data = null;
+		data = null;	
 		next = null;		
 	}
 	
 	public MyListNode(String data) {	//데이터를 받는 생성자
-		this.data = data;
+		this.data = data;				//데이터를 뒤에 하나씩 넣을때 사용
 		this.next = null;
 	}
 	
 	public MyListNode(String data, MyListNode link) {	//데이터와 다음 노드를 가르키는 생성자
-		this.data = data;
+		this.data = data;				//특정 데이터를 원하는 위치에 삽입 혹은 제거
 		this.next = link;
 	}
 	
